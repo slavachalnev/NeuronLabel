a0318197f7744eab23d040ce89ec6ee1c17d10ba
Slava Chalnev <slava.chalnev@gmail.com>
2023-04-21 12:26:31 +1000

    Separate into tokens

--- src/app.js
+++ src/app.js
@@ -8,28 +8,39 @@ const data = [
     "neuron_id": 1,
     "snippets": [
       {
-        "text": "This is a text snippet with an activated token.",
-        "activations": [0, 0, 0.5, 0, 0, 0, 0]
+        "tokens": ["This", " is", " a", " text", " snippet", " with", " an", " activated", " token."],
+        "activations": [0, 0, 0.5, 0, 0, 0, 0, 0, 0]
       },
       {
-        "text": "A different activated token.",
-        "activations": [0, 0.8, 0.5]
+        "tokens": ["A", " different", " activated", " token."],
+        "activations": [0, 0.8, 0.5, 0]
+      }
+    ]
+  },
+  {
+    "neuron_id": 2,
+    "snippets": [
+      {
+        "tokens": ["la", "la", "la"],
+        "activations": [0.2, 0, 0.5]
       }
     ]
   }
 ];
 
-function renderSnippets() {
-  data[0].snippets.forEach((snippet) => {
+function displaySnippets(snippets) {
+  snippets.forEach((snippet) => {
     const snippetDiv = document.createElement("div");
     snippetDiv.className = "snippet";
-    snippet.text.split(" ").forEach((word, index) => {
-      const token = document.createElement("span");
-      token.className = "token";
-      token.style.backgroundColor = `rgba(255, 0, 0, ${snippet.activations[index]})`;
-      token.textContent = word + " ";
-      snippetDiv.appendChild(token);
+
+    snippet.tokens.forEach((token, index) => {
+      const tokenElement = document.createElement("span");
+      tokenElement.className = "token";
+      tokenElement.style.backgroundColor = `rgba(255, 0, 0, ${snippet.activations[index]})`;
+      tokenElement.textContent = token;
+      snippetDiv.appendChild(tokenElement);
     });
+
     snippetsContainer.appendChild(snippetDiv);
   });
 }
@@ -48,29 +59,28 @@ downloadResultsButton.addEventListener("click", () => {
 });
 
 function saveResult(choice) {
-    const result = {
-      neuron_id: data[0].neuron_id,
-      choice: choice
-    };
-  
-    const resultsKey = 'neuron_results';
-    let results = JSON.parse(localStorage.getItem(resultsKey) || '[]');
-    results.push(result);
-    localStorage.setItem(resultsKey, JSON.stringify(results));
-  }
+  const result = {
+    neuron_id: data[0].neuron_id,
+    choice: choice
+  };
+
+  const resultsKey = 'neuron_results';
+  let results = JSON.parse(localStorage.getItem(resultsKey) || '[]');
+  results.push(result);
+  localStorage.setItem(resultsKey, JSON.stringify(results));
+}
 
 function downloadResults() {
-    const resultsKey = 'neuron_results';
-    const results = JSON.parse(localStorage.getItem(resultsKey) || '[]');
-  
-    const a = document.createElement("a");
-    a.href = URL.createObjectURL(new Blob([JSON.stringify(results, null, 2)], { type: "application/json" }));
-    a.download = "results.json";
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
+  const resultsKey = 'neuron_results';
+  const results = JSON.parse(localStorage.getItem(resultsKey) || '[]');
 
-renderSnippets();
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(new Blob([JSON.stringify(results, null, 2)], { type: "application/json" }));
+  a.download = "results.json";
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
 
+displaySnippets(data[0].snippets);
